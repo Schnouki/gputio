a97958c1582074385ac1a7861bc1e949972e0951
Thomas Jost <thomas.jost@gmail.com>
2010-11-15 16:36:11 +0100

    Add file icons

--- gputio.py
+++ gputio.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
 
+import mimetypes
 import putio
 
 import pygtk
 pygtk.require('2.0')
+import gio
 import glib
 import gtk
 
@@ -17,7 +19,7 @@ class GPutIO(object):
         win.add(hbox)
 
         # Tree store
-        self.tree = gtk.TreeStore(str, int)
+        self.tree = gtk.TreeStore(str, int, gtk.gdk.Pixbuf)
 
         # Scrolled window (for the tree view)
         sw = gtk.ScrolledWindow()
@@ -31,19 +33,26 @@ class GPutIO(object):
         self.tv.set_search_column(0)
         self.tv.set_rules_hint(True)
 
+        # Cell renderers
+        cell_txt = gtk.CellRendererText()
+        cell_txt_right = gtk.CellRendererText()
+        cell_txt_right.set_property("xalign", 1.0)
+        cell_pb = gtk.CellRendererPixbuf()
+
         # Add columns
-        cell = gtk.CellRendererText()
-        tvc = gtk.TreeViewColumn("Name", cell, text=0)
-        self.tv.append_column(tvc)
+        tvc = gtk.TreeViewColumn("Name")
+        tvc.pack_start(cell_pb, expand=False)
+        tvc.pack_start(cell_txt)
+        tvc.add_attribute(cell_txt, "text", 0)
+        tvc.add_attribute(cell_pb, "pixbuf", 2)
         tvc.set_expand(True)
         tvc.set_sort_column_id(0)
-
-        cell = gtk.CellRendererText()
-        cell.set_property("xalign", 1.0)
-        tvc = gtk.TreeViewColumn("Size", cell)
-        tvc.set_cell_data_func(cell, self._render_size)
         self.tv.append_column(tvc)
+
+        tvc = gtk.TreeViewColumn("Size", cell_txt_right)
+        tvc.set_cell_data_func(cell_txt_right, self._render_size)
         tvc.set_sort_column_id(1)
+        self.tv.append_column(tvc)
 
         # Buttons box
         bbox = gtk.VButtonBox()
@@ -78,11 +87,23 @@ class GPutIO(object):
 
     def _get_folder(self, root, parent):
         items = self.api.get_items(parent_id=root)
+        theme = gtk.icon_theme_get_default()
         for it in items:
-            tree_iter = self.tree.append(parent, (it.name, int(it.size)))
             if it.is_dir:
+                pb = theme.load_icon("folder", 16, 0)
+                tree_iter = self.tree.append(parent,
+                                             (it.name, int(it.size), pb))
                 glib.idle_add(self._get_folder, it.id, tree_iter,
                               priority=glib.PRIORITY_LOW)
+
+            else:
+                pb = None
+                (file_type, encoding) = mimetypes.guess_type(it.name)
+                if file_type is not None:
+                    icon_names = gio.content_type_get_icon(file_type).get_names()
+                    pb = theme.choose_icon(icon_names, 16, 0).load_icon()
+                tree_iter = self.tree.append(parent,
+                                             (it.name, int(it.size), pb))
 
             self.tv.queue_draw()
             self.tv.window.process_updates(True)
